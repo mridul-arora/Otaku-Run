d7b94b288687ef1efee1c5ddd2f1f1b944913145
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-23 21:30:34 +0530

    new folders and header.php

---
+++ Assets/parts/header.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <link ref="stylesheet" href="../css/header.css">
+</head>
+
+<body>
+
+    <header id="header">
+        <a href="/"><img src="/assets/images/a.jpg" style="height: 4vh; position: absolute" /><span id="brand"><strong>Pattern</strong></span></a>
+        <div class="right">
+            <ul>
+                <li><a href='/Home/index.php' class='nav'>Home</a></li>
+                <li><a href='/Login/index.php' class='nav'>Log in</a></li>
+                <li><a href='/Register/index.php' class='nav'>Register</a></li>
+                <li><a href='/Code/index.php' class='nav'>Code</a></li>
+                <li><a href='/logout' class='nav'>Logout</a></li>
+                <!--            <li><a href='/leaderboard' class='nav'>Leaderboard</a></li> -->
+
+
+            </ul>
+        </div>
+    </header>
+</body>
+
+</html>
---
+++ Home/index.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>
+    Home-Prodigy
+    </title>
+</head>
+    <body>
+
+    </body>
+
+</html>
