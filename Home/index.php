50395ebb91979eedae572e1c67ceced60c7a9d5b
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-24 23:08:04 +0530

    home.php white bgcolor

--- Home/index.php
+++ Home/index.php
@@ -4,18 +4,22 @@
     <title>
         Home-Prodigy123
     </title>
-   </head>
+    <link rel="stylesheet" href="../Assets/css/home.css"/>
+</head>
 
 <body>
     <?php include '../Assets/parts/header.php';?>
     <link rel="stylesheet" href="../Assets/css/header.css">
-   <?php include '../Assets/parts/includes.php'; ?>
-
-    <p>This is an online technical event which comprises of two rounds.</p>
-    <p>1st round:</p>
-    <p>4 Patterns will be shown one after another. Codes for these patterns have to be submitted. There will be a time limit of 5 minutes for each pattern. 10 participants will qualify will qualify for the next round.</p>
-    <p>2nd round:</p>
-    <p>This is a time based event. Each question containing a code will be displayed for 5 minutes, the participants who submits the correct output first gets the most points and accordingly marking will be done based on their submission time. There will be 5-10 questions.. The participants with maximum score wins.</p>.
+    <?php include '../Assets/parts/includes.php'; ?>
+    <article id="border_center_box">
+    <article id="center_box">
+        <p>This is an online technical event which comprises of two rounds.<br>
+        1st round:<br>
+        4 Patterns will be shown one after another. Codes for these patterns have to be submitted. There will be a time limit of 5 minutes for each pattern. 10 participants will qualify will qualify for the next round.<br>
+        2nd round:<br>
+        This is a time based event. Each question containing a code will be displayed for 5 minutes, the participants who submits the correct output first gets the most points and accordingly marking will be done based on their submission time. There will be 5-10 questions.. The participants with maximum score wins.</p>
+    </article>
+        </article>
 </body>
 
 </html>
