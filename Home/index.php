2976c3ae1ef7411129c91a135cf614a1040a240a
500060750 <500060750@stu.upes.ac.in>
2017-12-24 06:58:43 -0800

    qwerty

--- Home/index.php
+++ Home/index.php
@@ -9,7 +9,12 @@
     </style>
 </head>
     <body>
-
+<p>This is an online technical event which comprises of two rounds.</p>
+<p>1st round:</p>
+<p>4 Patterns will be shown one after another. Codes for these patterns have to be submitted.
+   There will be a time limit of 5 minutes for each pattern. 10 participants will qualify will qualify for the next round.</p>
+<p>2nd round:</p>
+<p>This is a time based event. Each question containing a code will be displayed for 5 minutes, the participants who submits the correct output first gets the most points and accordingly marking will be done based on their submission time. There will be 5-10 questions.. The participants with maximum score wins.</p>
     </body>
 
 </html>
