33c522ab436edf00247027127be52aaabd404b13
500060750 <500060750@stu.upes.ac.in>
2017-12-24 09:27:51 -0800

    abc

--- Register/index.php
+++ Register/index.php
@@ -10,6 +10,23 @@
     <?php include '../Assets/parts/header.php';?>
     <link rel="stylesheet" href="../Assets/css/header.css">
    <?php include '../Assets/parts/includes.php'; ?>
+    <form action="/action_page.php">
+        First name:<br>
+        <input type="text"
+               name="firstname">
+        <br>
+        Last name:<br>
+        <input type="text"
+               name="lastname">
+        <br>
+        User name:<br>
+        <input type="text"
+               name="username">
+        <br>
+        Contact No.:<br>
+        <input type="text"
+
+    </form>
 </body>
 
 </html>
