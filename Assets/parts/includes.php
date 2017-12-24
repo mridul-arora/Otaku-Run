892747d69a5b21619703ec49743db02fdbdc9345
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-24 21:17:59 +0530

    CSS's code,login,register <includes>

--- Assets/parts/includes.php
+++ Assets/parts/includes.php
@@ -1,2 +1,7 @@
-<?php include '../css/header.css';?>
-<?php include '../css/home.css';?>
+<link rel="stylesheet" href="../css/header.css">
+<link rel="stylesheet" href="../css/home.css">
+<link rel="stylesheet" href="../css/code.css">
+<link rel="stylesheet" href="../css/login.css">
+<link rel="stylesheet" href="../css/register.css">
+
+
---
+++ Register/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>
+    Register -Pattern
+    </title>
+    </head>
+    <body>
+
+    </body>
+</html>
