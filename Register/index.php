e800dcbac51a433cceda0580736543a27bba37cf
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-24 23:45:50 +0530

    look at this

--- Register/index.php
+++ Register/index.php
@@ -7,38 +7,31 @@
 </head>
 
 <body>
-    <?php include '../Assets/parts/header.php';?>
-    <link rel="stylesheet" href="../Assets/css/header.css">
-   <?php include '../Assets/parts/includes.php'; ?>
-    <form action="/action_page.php">
-        First name:<br>
-        <input type="text"
-               name="firstname">
-        <br>
-        Last name:<br>
-        <input type="text"
-               name="lastname">
-        <br>
-        User name:<br>
-        <input type="text"
-               name="username">
-        <br>
-        Contact No.:<br>
-        <input type="text"
-               name="number">
-        <br>
-        Email_id:<br>
-        <input type="email"
-               name="email">
-        <br>
-        Password:<br>
-        <input type="password"
-               name="psw">
-        <br>
-        <br>
-        <input type="submit">
+   <!-- /*<?php //include '../Assets/parts/header.php';?>
+    <link rel="stylesheet" href="../Assets/css/header.css">-->
+    <?php include '../Assets/parts/includes.php'; ?>
+    <div id="reg_wrapper">
+        <form action="/action_page.php">
+            <article class="reg">
+                <input class="reg_info" type="text" placeholder="First Name" name="firstname" />
+                <input class="reg_info" type="text" placeholder="Last Name" name="lastname" />
+            </article>
+            Username:<br>
+            <input type="text" name="username">
+            <br> Contact No.:<br>
+            <input type="text" name="number">
+            <br> Email_id:
+            <br>
+            <input type="email" name="email">
+            <br> Password:
+            <br>
+            <input type="password" name="psw">
+            <br>
+            <br>
+            <input type="submit">
 
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
