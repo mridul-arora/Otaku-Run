00df8324f9150f64455d1f50d92074639b875b57
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-25 22:14:21 +0530

    merge rectify

--- Code/index.php
+++ Code/index.php
@@ -10,29 +10,19 @@
     <?php include '../Assets/parts/header.php';?>
     <link rel="stylesheet" href="../Assets/css/header.css" />
     <link rel="stylesheet" href="../Assets/css/code.css" />
-<<<<<<< HEAD
-    <center>
-    <form action="code.php" method="post">
-    <form action="/action_page.php">
-  <textarea name="message" rows="20" cols="200">WRITE CODE HERE.</textarea>
-     <article class="code">
-     <input class="code_sub" type="submit" value="Submit" name="submit" />
-    </article>
-</form>
-</form>
-</center>
-=======
+
+
     <div id="code_wrapper">
         <center>
             <article id="code_box">
                 <form action="code.php" method="post">
                     <article class="code">
-                        <input class="code_sub" type="submit" value="Submit" name="submit" />
+                         <textarea name="message" rows="20" cols="100">WRITE CODE HERE.</textarea>
+                        <input class="code_sub" type="submit" value="Compile & Run" name="submit" />
                     </article>
                 </form>
             </article>
         </center>
     </div>
->>>>>>> origin/master
 </body>
 </html>
