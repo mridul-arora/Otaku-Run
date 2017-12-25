05a239fbb9f1a75117b3a6c535b1a060d8c6ec35
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-26 00:18:35 +0530

    backups

---
+++ Assets/css/index_code_backup.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <title>
+        Code -Pattern...
+    </title>
+</head>
+
+<body>
+    <?php include '../Assets/parts/header.php';?>
+    <link rel="stylesheet" href="../Assets/css/header.css" />
+    <link rel="stylesheet" href="../Assets/css/code.css" />
+
+    <div id="code_wrapper">
+        <center>
+
+            <article id="code_box">Question.
+                 <div id="ques_box">
+             <article id="ques">
+                   <pre>
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+                    </pre>
+                </article>
+                </div>
+                <form action="code.php" method="post">
+                    <article class="code">
+                        <textarea name="writecode" id="code_here" placeholder="WRITE CODE HERE."></textarea>
+                        <input class="code_sub" type="submit" value="Compile & Run" name="submit" />
+                    </article>
+                </form>
+            </article>
+        </center>
+    </div>
+
+</body>
+
+</html>
