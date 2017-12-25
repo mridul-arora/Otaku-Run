094e8938799b70f96b5dc9792a9c66659e9166b1
JYOTI ARORA <JYOTI ARORA@upes-500061036.DDN.UPES.AC.IN>
2017-12-26 02:04:35 +0530

    navigation bar

--- Assets/functions/register.php
+++ Assets/functions/register.php
@@ -7,3 +7,17 @@
     $number = $_POST['number'];
 
 
+ $con = mysqli_connect('localhost','root','');
+    mysqli_query($con, "CREATE DATABASE IF NOT EXISTS ucode;");
+    mysqli_select_db($con,'ucode');
+    mysqli_query($con, "CREATE TABLE IF NOT EXISTS users ( fname TEXT NOT NULL , lname TEXT NOT NULL , mname TEXT, sap INT NOT NULL , username TEXT NOT NULL , pass TEXT NOT NULL , email TEXT NOT NULL , phone BIGINT NOT NULL , branch TEXT , yr TEXT, PRIMARY KEY (username(40)));");
+
+    $query = "insert into users VALUES ( '$fname', '$lname', '$mname', $sap, '$username', '$password', '$email', $number, '$branch', '$year');";
+    mysqli_query($con, $query);
+
+    $row = mysqli_fetch_array(mysqli_query($con,"select count(*) from leaderboard;"));
+    $count = $row[0]+1;
+    if(strpos($email,"@stu.upes.ac.in")) mysqli_query($con, "insert into billboard VALUES ( '$username', $count, 0);");
+
+    header('location: /');
+?>
