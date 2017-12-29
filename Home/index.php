693b2baf3cc999fdc337ffa68716374c729e4392
500060750 <500060750@stu.upes.ac.in>
2017-12-29 07:16:30 -0800

    jhdg

--- Home/index.php
+++ Home/index.php
@@ -16,7 +16,7 @@
             1st round:</b><br>
         4 Patterns will be shown one after another. Codes for these patterns have to be submitted. There will be a time limit of 5 minutes for each pattern. 10 participants will qualify will qualify for the next round.<br>
             <b> 2nd round:</b><br>
-        This is a time based event. Each question containing a code will be displayed for 5 minutes, the participants who submits the correct output first gets the most points and accordingly marking will be done based on their submission time. There will be 5-10 questions.. The participants with maximum score wins.</p>
+        This is a time based event. Each question containing a code will be displayed for 5 minutes, the participants who submits the correct output first gets the most points and accordingly marking will be done based on their submission time. There will be 5-10 questions.. The participants with maximum score wins..</p>
     </article>
         </div>
 </body>
